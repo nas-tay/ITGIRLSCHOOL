9664d7ce551be13c4e3feb6453c16e43e88e4946
Anastasia Karzanova <anastasiakarzanova@Anastasias-MacBook-Pro.local>
2022-05-04 10:49:50 +0300

    modify week2

---
+++ week2/index.js
@@ -0,0 +1,4 @@
+function changeMe() {
+    let  catImage = document.getElementById('catImage');
+    catImage.src = "https://magazine.skyeng.ru/wp-content/uploads/2019/03/shutterstock_745196731.jpg";
+}

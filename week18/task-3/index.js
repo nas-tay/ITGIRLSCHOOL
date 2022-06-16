0fd3d90ee823bd4356f36f8854fae44b38e78919
Anastasia Karzanova <anastasiakarzanova@Anastasias-MacBook-Pro.local>
2022-06-16 19:03:40 +0300

    week18 complete

--- week18/task-3/index.js
+++ week18/task-3/index.js
@@ -1,6 +1,7 @@
 function addCommentGlobal() {
     let comment;
     let userName = document.querySelector("#userName");
+    let userPic = document.querySelector("#userPicture");
 
     //Замыкание
     function addComment() {
@@ -9,21 +10,31 @@ function addCommentGlobal() {
         // Спам-фильтр
         let commentChecked = comment.value.replace(/xxx|viagra/gi, "***");
 
-        localStorage.setItem(userName.value, commentChecked);
+        localStorage.setItem("name", userName.value);
+        localStorage.setItem("image", userPic.value);
+        localStorage.setItem("comment", comment.value);
 
-        // // Выводим коммент в секции комментов
-        // let newComment = document.createElement("div");
-        // newComment.innerHTML = `
-        // User: ${commentChecked} <br><br>
-        // `;
-        // document.querySelector(".commentContainer").append(newComment);
+        // Выводим коммент в секции комментов
+        let newComment = document.createElement("div");
+        newComment.innerHTML = `
+        ${userName.value}: ${commentChecked} <br><br>
+        `;
+        document.querySelector(".commentContainer").append(newComment);
 
-        // Удаление дефолтной строки в секции комментов
-        // if (newComment.innerHTML == "") {
-        //     document.querySelector(".none").style.display = "block";
-        // } else {
-        //     document.querySelector(".none").style.display = "none";
-        // }
+        if (userPic.value) {
+            newComment.innerHTML = `
+            <img src="${userPic.value}" style="width: 50px; height: 50px" alt="user image"> <br>
+        ${userName.value}: ${commentChecked} <br><br>
+        `;
+            document.querySelector(".commentContainer").append(newComment);
+        }
+
+        //Удаление дефолтной строки в секции комментов
+        if (newComment.innerHTML == "") {
+            document.querySelector(".none").style.display = "block";
+        } else {
+            document.querySelector(".none").style.display = "none";
+        }
 
         // Очистка textarea
         comment.value = "";
@@ -32,3 +43,20 @@ function addCommentGlobal() {
 }
 
 document.querySelector("#send").addEventListener("click", addCommentGlobal);
+
+document.addEventListener("DOMContentLoaded", () => {
+    let name = localStorage.getItem("name");
+    let image = localStorage.getItem("image");
+    let comment = localStorage.getItem("comment");
+
+    //Запоминание имени и картинки
+    document.querySelector("#userName").value = name;
+    document.querySelector("#userPicture").value = image;
+
+    if (name) {
+        document.querySelector(".commentContainer").innerHTML = `
+            <img src="${image}" style="width: 50px; height: 50px" alt="user image"> <br>
+        ${name}: ${comment} <br><br>
+        `;
+    }
+});
